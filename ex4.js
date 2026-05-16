b8e382c00578c061e47a28a89789b6daaf22113d
GunnamKarthik <145652156+GunnamKarthik@users.noreply.github.com>
2026-05-16 12:50:34 +0100

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -14,3 +14,28 @@
 //keep code DRY (Don't repeat yourself)
 
 //if you're finished, code it into a page
+function sumOfMultiples(factors,numbers){
+  let sum = 0;
+  for (let num of numbers){
+    for(let factor of factors){
+      if (num % factor === 0){
+        sum +=num;
+        break;
+      }
+    }
+  }
+  return sum;
+}
+function processfactors(){
+  let input = document.getElementById("inputStr").value;
+  let parts=input.split(":");
+  if(parts.length!==2){
+    document.getElementById("output").innerText="invalid format!";
+    return;
+  }
+  let factors =parts[0].trim().split(" ").map(Number);
+  let multiples =parts[1].trim().split(" ").map(Number);
+  let result = sumOfMultiples(factors,multiples):
+  document.getElementById("output").innerText=result+":"+factors.join(" ")+":"+multiples.join(" ");
+}
+  
